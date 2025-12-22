4ec212e0e28938aa967bd28e0f9672a74f725845
Petlevannaya-Olga <olga.g.afonina@gmail.com>
2025-12-22 22:01:15 +0300

    fix

--- DirectoryService/src/DirectoryService.Presentation/Program.cs
+++ DirectoryService/src/DirectoryService.Presentation/Program.cs
@@ -4,8 +4,6 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
-//builder.Services.AddScoped<ApplicationDbContext>(_ =>
-//    new ApplicationDbContext(builder.Configuration.GetConnectionString("DirectoryService")!));
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(
